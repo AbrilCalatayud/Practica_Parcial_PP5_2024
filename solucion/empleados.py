b8ef85e50e979d99e06f8f2a56e8510b4d766691
Abril Nataly Calatayud <abril.calatayud.contacto@gmail.com>
2026-09-17 13:30:14 -0300

    Implementé la clase Categoria y la clase Contratado

--- solucion/empleados.py
+++ solucion/empleados.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Empleado:
     def __init__(self, nombre, apellido, dni, categoria):
         self.nombre = nombre
@@ -14,3 +16,17 @@ class Empleado:
 
     def precarizar(self, nivel):
         self.categoria = Contratado(nivel)
+
+class Categoria(ABC):
+
+    @abstractmethod
+    def calcular_sueldo(self, horas_trabajadas_este_mes):
+        pass
+
+class Contratado(Categoria):
+    def __init__(self, horas_minimas_diarias, costo_hora):
+        self.horas_minimas_diarias = horas_minimas_diarias
+        self.costo_hora = costo_hora
+
+    def calcular_sueldo(self, horas_trabajadas_este_mes):
+        return sum(map(lambda x: x * self.costo_hora, filter(lambda x: x > self.horas_minimas_diarias, horas_trabajadas_este_mes)))
